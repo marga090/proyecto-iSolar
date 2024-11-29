7f500d8cfb485800fb86fcd254e660afea656b76
Angel <angeldzrz3103@gmail.com>
2024-11-29 10:13:06 +0100

    creacion de modelo relacional, tablas e inserts para la bd de prueba

---
+++ modelo logico.sql
@@ -0,0 +1,27 @@
+-- MODELO LÓGICO DE LA BD
+CLIENTE(id_cliente, nombre, direccion, localidad, provincia, telefono, observaciones, correo, modo_captacion)
+	PK(id_cliente)
+
+VIVIENDA(id_vivienda, n_personas, bombona, gas, termo_electrico, placas_termicas, id_vivienda)
+	PK(id_vivienda)
+	FK(id_cliente -> CLIENTE)
+
+RECIBO(id_recibo, importe_luz, importe_gas, id_vivienda)
+	PK(id)
+	FK(id_vivienda -> VIVIENDA)
+
+TRABAJADOR(id_trabajador, nombre)
+	PK(id_trabajador)
+
+CAPTADOR (id_trabajador)
+	PK (id_trabajador)
+	FK (id_trabajador -> TRABAJADOR)
+
+COMERCIAL (id_trabajador)
+	PK (id_trabajador)
+	FK (id_trabajador -> TRABAJADOR)
+
+VISITA (id_visita, fecha, hora, tipo, resultado, id_vivienda, id_comercial)
+	PK(id_visita, id_vivienda, id_comercial)
+	FK(id_vivienda -> VIVIENDA)
+	FK(id_comercial -> COMERCIAL)
