5dd6d2c03fd90c79b9593b237ad2a51e2dbec3bb
Angel <angeldzrz3103@gmail.com>
2024-12-12 14:35:17 +0100

    correccion del entidad-relacion

--- modelo logico.sql
+++ modelo logico.sql
@@ -1,16 +1,20 @@
 -- MODELO LÓGICO DE LA BD
-CLIENTE(id_cliente, nombre, direccion, localidad, provincia, telefono, observaciones, correo, modo_captacion)
+CLIENTE(id_cliente, nombre, telefono, correo, modo_captacion, observaciones)
 	PK(id_cliente)
 
-VIVIENDA(id_vivienda, n_personas, bombona, gas, termo_electrico, placas_termicas, id_vivienda)
+DIRECCION(id_direccion, calle, numero, localidad, provincia, codigo_postal, id_cliente)
+	PK(id_direccion)
+	FK(id_cliente)
+
+VIVIENDA(id_vivienda, n_personas, bombona, gas, termo_electrico, placas_termicas, id_direccion)
 	PK(id_vivienda)
-	FK(id_cliente -> CLIENTE)
+	FK(id_direccion -> DIRECCION)
 
 RECIBO(id_recibo, importe_luz, importe_gas, id_vivienda)
-	PK(id)
+	PK(id_recibo)
 	FK(id_vivienda -> VIVIENDA)
 
-TRABAJADOR(id_trabajador, nombre)
+TRABAJADOR(id_trabajador, nombre, telefono)
 	PK(id_trabajador)
 
 CAPTADOR (id_trabajador)
@@ -22,6 +26,6 @@ COMERCIAL (id_trabajador)
 	FK (id_trabajador -> TRABAJADOR)
 
 VISITA (id_visita, fecha, hora, tipo, resultado, id_vivienda, id_trabajador)
-	PK(id_visita, id_vivienda, id_comercial)
+	PK(id_visita)
 	FK(id_vivienda -> VIVIENDA)
 	FK(id_trabajador -> COMERCIAL)
