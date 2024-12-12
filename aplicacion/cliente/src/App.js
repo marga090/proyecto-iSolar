930da63d2488fb41753bff267df0a1a16dc753c1
Angel <angeldzrz3103@gmail.com>
2024-12-12 14:07:53 +0100

    creacion del metodo listar clientes y mejorar el diseño del formulario, ademas de añadir restricciones y excepciones en el backend

--- aplicacion/cliente/src/App.js
+++ aplicacion/cliente/src/App.js
@@ -1,10 +1,12 @@
+//importamos el css
 import './App.css';
+// importamos los Estados para poder obtener los valores introducidos por el usuario
 import { useState } from "react";
+// importamos Axio, nos permite hacer sencillas las operaciones como cliente HTTP
 import Axios from "axios";
 
 function App() {
-
-  const [idCliente, setIdCliente] = useState("");
+  // creamos las constantes para obtener los valores de los campos del furmulario
   const [nombreContacto, setNombreContacto] = useState("");
   const [direccionContacto, setDireccionContacto] = useState("");
   const [localidadContacto, setLocalidadContacto] = useState("");
@@ -13,10 +15,13 @@ function App() {
   const [observacionesContacto, setObservacionesContacto] = useState("");
   const [correoContacto, setCorreoContacto] = useState("");
   const [modoCaptacion, setModoCaptacion] = useState("");
+  // creamos una lista de clientes
+  const [listaClientes, setClientes] = useState([]);
 
+  // metodo para crear clientes
   const add = () =>
+    // llamamos al metodo crear
     Axios.post("http://localhost:3001/create", {
-      idCliente: idCliente,
       nombreContacto: nombreContacto,
       direccionContacto: direccionContacto,
       localidadContacto: localidadContacto,
@@ -33,82 +38,103 @@ function App() {
         console.error("Error al enviar los datos:", error);
       });
 
+  // metodo para obtener clientes
+  const getClientes = () =>
+    // llamamos a clientes
+    Axios.get("http://localhost:3001/clientes")
+      // cuando obtenga la respuesta lo guarde en una variable
+      .then((response) => {
+        // vienen todos los datos de clientes de la aplicacion
+        setClientes(response.data);
+      })
+      .catch((error) => {
+        console.error("Error al mostrar los datos:", error);
+      }
+      );
 
+  // este es el html visible en la web
   return (
     <div className="App">
       <h1>Formulario de contacto</h1>
-      <div className="datos">
-        <label>ID cliente: <input
-          onChange={(event) => {
-            setIdCliente(event.target.value);
-          }}
-          type="text"></input></label>
 
-        <label>Nombre completo del contacto: <input
-          onChange={(event) => {
-            setNombreContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+      <div className="contenedorFormulario">
+        <div className="datosCliente">
+          <label>Nombre completo del contacto:</label>
+          <input
+            onChange={(event) => setNombreContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce el nombre completo del cliente"
+          />
 
-        <label>Dirección del contacto: <input
-          onChange={(event) => {
-            setDireccionContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Dirección del contacto:</label>
+          <input
+            onChange={(event) => setDireccionContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce la dirección del cliente"
+          />
 
-        <label>Localidad del contacto: <input
-          onChange={(event) => {
-            setLocalidadContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Localidad del contacto:</label>
+          <input
+            onChange={(event) => setLocalidadContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce la localidad del cliente"
+          />
 
-        <label>Provincia del contacto: <input
-          onChange={(event) => {
-            setProvinciaContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Provincia del contacto:</label>
+          <input
+            onChange={(event) => setProvinciaContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce la provincia del cliente"
+          />
 
-        <label>Teléfono de contacto: <input
-          onChange={(event) => {
-            setTelefonoContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Teléfono de contacto:</label>
+          <input
+            onChange={(event) => setTelefonoContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce el teléfono del cliente"
+          />
 
-        <label>Observaciones del contacto: <input
-          onChange={(event) => {
-            setObservacionesContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Observaciones del contacto:</label>
+          <input
+            onChange={(event) => setObservacionesContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce observaciones del cliente si las hay"
+          />
 
-        <label>Correo del contacto: <input
-          onChange={(event) => {
-            setCorreoContacto(event.target.value);
-          }}
-          type="text"></input>
-        </label>
+          <label>Correo del contacto:</label>
+          <input
+            onChange={(event) => setCorreoContacto(event.target.value)}
+            type="text"
+            placeholder="Introduce el correo electrónico del cliente"
+          />
 
-        <label>Modo de captación: <select
-          onChange={(event) => {
-            setModoCaptacion(event.target.value);
-          }}
-          value={modoCaptacion}>
-          <option value="">Selecciona un modo de captación</option>
-          <option value="Captador">Captador</option>
-          <option value="Telemarketing">Telemarketing</option>
-          <option value="Referido">Referido</option>
-          <option value="Propia">Propia</option>
-        </select></label>
+          <label>Modo de captación:</label>
+          <select
+            onChange={(event) => setModoCaptacion(event.target.value)}
+            value={modoCaptacion}
+          >
+            <option value="">Selecciona un modo de captación</option>
+            <option value="Captador">Captador</option>
+            <option value="Telemarketing">Telemarketing</option>
+            <option value="Referido">Referido</option>
+            <option value="Propia"> Captación propia</option>
+          </select>
 
-        <button onClick={add}>Registrar</button>
+          <button onClick={add}>Registrar</button>
+        </div>
+      </div>
+
+      <div className="listaClientes">
+        <button onClick={getClientes}>Listar clientes</button>
+        <div className="clientes-list">
+          {listaClientes.map((valor) => {
+            return <div key={valor.id_cliente} className="cliente-item">{valor.nombre}</div>;
+          })}
+        </div>
       </div>
     </div>
   );
+
 }
 
 export default App;
--- aplicacion/servidor/index.js
+++ aplicacion/servidor/index.js
@@ -1,11 +1,17 @@
+// importamos express, para las conexiones HTTP y lo almacenamos
 const express = require("express");
 const app = express();
+// importamos mysql
 const mysql = require("mysql");
+// importamos cors, que permite realizar solicitudes y transferencias de datos entre servidores 
 const cors = require("cors");
 
+// permite que el frontend en localhost:3000 haga solicitudes al backend
 app.use(cors({ origin: "http://localhost:3000" }));
+// convierte las peticiones en formato .json
 app.use(express.json());
 
+// creamos la conexion con mysql workbench
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
@@ -13,47 +19,79 @@ const db = mysql.createConnection({
     database: "prueba"
 });
 
+// creamos la peticion de GUARDAR
 app.post("/create", (req, res) => {
+
+    // cuando se haga la consulta y obtengamosla respuesta...
     console.log("Datos recibidos del frontend:", req.body);
 
-    const { 
-        idCliente, 
-        nombreContacto, 
-        direccionContacto, 
-        localidadContacto, 
-        provinciaContacto, 
-        telefonoContacto, 
-        observacionesContacto, 
-        correoContacto, 
-        modoCaptacion 
+    // creamos las constantes de los campos y en ella almacemanos la informacion que llega del body
+    const {
+        nombreContacto,
+        direccionContacto,
+        localidadContacto,
+        provinciaContacto,
+        telefonoContacto,
+        observacionesContacto,
+        correoContacto,
+        modoCaptacion
+        // lo que llega de la solicitud
     } = req.body;
 
-    // Verificar que los valores no sean nulos o vacíos
-    if (!direccionContacto || !telefonoContacto || !correoContacto || !modoCaptacion) {
-        return res.status(400).json({ error: "Faltan datos obligatorios." });
+    // verificamos que los campos no sean nulos o vacíos
+    if (!nombreContacto || !direccionContacto || !localidadContacto || !provinciaContacto || !telefonoContacto || !correoContacto || !modoCaptacion) {
+        return res.status(400).json({ error: "Faltan datos que son obligatorios." });
     }
 
-    // Validar el valor de modoCaptacion
+    // validamos el valor de modoCaptacion
     const validCaptationModes = ["Captador", "Telemarketing", "Referido", "Propia"];
     if (!validCaptationModes.includes(modoCaptacion)) {
         return res.status(400).json({ error: "Modo de captación inválido." });
     }
 
-    // Insertar en la base de datos
+    // insertar valores en la base de datos
     db.query(
-        'INSERT INTO cliente(id_cliente, nombre, direccion, localidad, provincia, telefono, observaciones, correo, modo_captacion) VALUES(?,?,?,?,?,?,?,?,?)',
-        [idCliente, nombreContacto, direccionContacto, localidadContacto, provinciaContacto, telefonoContacto, observacionesContacto, correoContacto, modoCaptacion],
+        // campos de las tablas de la bd
+        'INSERT INTO cliente(nombre, direccion, localidad, provincia, telefono, observaciones, correo, modo_captacion) VALUES(?,?,?,?,?,?,?,?)',
+        // variables creadas en la aplicacion
+        [nombreContacto, direccionContacto, localidadContacto, provinciaContacto, telefonoContacto, observacionesContacto, correoContacto, modoCaptacion],
+
+        // gestionamos los errores
         (err, result) => {
             if (err) {
-                console.error("Error en la base de datos:", err.sqlMessage);  // Muestra el mensaje de error específico de MySQL
+                // muestra el error de sql
+                console.error("Error en la base de datos:", err.sqlMessage);
                 return res.status(500).json({ error: err.sqlMessage || "Hubo un error al guardar los datos en la base de datos" });
             }
             res.status(200).json({ message: "Datos guardados correctamente", result });
         }
-    );    
+    );
+});
+
+// creamos la peticion de LISTAR
+app.get("/clientes", (req, res) => {
+
+    // cuando se haga la consulta y obtengamosla respuesta...
+    console.log("Clientes listados", req.body);
+
+    // listar valores de la tabla cliente de la base de datos
+    db.query(
+        // campos de las tablas de la bd
+        'SELECT * FROM cliente',
+        // gestionamos los errores
+        (err, result) => {
+            if (err) {
+                // muestra el error de sql
+                console.error("Error en la base de datos:", err.sqlMessage);
+                return res.status(500).json({ error: err.sqlMessage || "Hubo un error al mostar los datos en la base de datos" });
+            }
+            res.send(result);
+        }
+    );
 });
 
 
+// mensaje para verificar que el backend esta funcionando correctamente y escuchando por el puerto 3001
 app.listen(3001, () => {
     console.log("Funcionando en el puerto 3001")
 })
