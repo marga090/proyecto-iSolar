4321804e6e7b7c3be535b0c4dfb4e32b1a17af6d
Angel <angeldzrz3103@gmail.com>
2025-01-15 13:44:29 +0100

    correccion de pequeños errores

--- aplicacion/cliente/src/pages/Formulario.jsx
+++ aplicacion/cliente/src/pages/Formulario.jsx
@@ -24,8 +24,8 @@ export default function Formulario() {
         tieneGas: "Sin datos",
         tieneTermoElectrico: "Sin datos",
         tienePlacasTermicas: "Sin datos",
-        importeLuz: 0.00,
-        importeGas: 0.00
+        importeLuz: 0,
+        importeGas: 0
     });
 
     const [errores, setErrores] = useState({});
