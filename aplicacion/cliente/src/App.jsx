20a908853d0f97aaee8c53aa562a1e819e2ea8f6
Angel <angeldzrz3103@gmail.com>
2025-02-20 14:31:21 +0100

    crear un archivo de rutas para app.jsx

--- aplicacion/cliente/src/App.jsx
+++ aplicacion/cliente/src/App.jsx
@@ -1,16 +1,9 @@
-import React from "react";
 import "./App.css";
 import Logo from "./images/logo.png";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Inicio from "./pages/Inicio";
-import Captador from "./pages/Captador";
-import Contacto from "./pages/Contacto";
-import Visita from "./pages/Visita";
-import Comercial from "./pages/Comercial";
-import Feedback from "./pages/Feedback";
-import Trabajador from "./pages/Trabajador";
+import routes from "./routesConfig";
 import PrivateRoute from "./components/PrivateRoute";
-import Administrador from "./pages/Administrador";
 
 function App() {
   return (
@@ -24,22 +17,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Inicio />} />
 
-        <Route element={<PrivateRoute allowedRoles={["Administrador"]} />}>
-          <Route path="/administradores" element={<Administrador />} />
-          <Route path="/administradores/trabajador" element={<Trabajador />} />
-        </Route>
-
-        <Route element={<PrivateRoute allowedRoles={["Captador", "Administrador"]} />}>
-          <Route path="/captadores" element={<Captador />} />
-          <Route path="/captadores/contacto" element={<Contacto />} />
-          <Route path="/captadores/visita" element={<Visita />} />
-        </Route>
-
-        <Route element={<PrivateRoute allowedRoles={["Comercial", "Administrador"]} />}>
-          <Route path="/comerciales" element={<Comercial />} />
-          <Route path="/comerciales/contacto" element={<Contacto />} />
-          <Route path="/comerciales/feedback" element={<Feedback />} />
-        </Route>
+        {routes.map(({ path, element, roles }) => (
+          <Route key={path} element={<PrivateRoute allowedRoles={roles} />}>
+            <Route path={path} element={element} />
+          </Route>
+        ))}
       </Routes>
     </BrowserRouter>
   );
---
+++ aplicacion/cliente/src/routesConfig.jsx
@@ -0,0 +1,52 @@
+import Captador from "./pages/Captador";
+import Contacto from "./pages/Contacto";
+import Visita from "./pages/Visita";
+import Comercial from "./pages/Comercial";
+import Feedback from "./pages/Feedback";
+import Trabajador from "./pages/Trabajador";
+import Administrador from "./pages/Administrador";
+
+const routes = [
+    {
+        path: "/administradores",
+        element: <Administrador />,
+        roles: ["Administrador"],
+    },
+    {
+        path: "/administradores/trabajador",
+        element: <Trabajador />,
+        roles: ["Administrador"],
+    },
+    {
+        path: "/captadores",
+        element: <Captador />,
+        roles: ["Captador", "Administrador"],
+    },
+    {
+        path: "/captadores/contacto",
+        element: <Contacto />,
+        roles: ["Captador", "Administrador"],
+    },
+    {
+        path: "/captadores/visita",
+        element: <Visita />,
+        roles: ["Captador", "Administrador"],
+    },
+    {
+        path: "/comerciales",
+        element: <Comercial />,
+        roles: ["Comercial", "Administrador"],
+    },
+    {
+        path: "/comerciales/contacto",
+        element: <Contacto />,
+        roles: ["Comercial", "Administrador"],
+    },
+    {
+        path: "/comerciales/feedback",
+        element: <Feedback />,
+        roles: ["Comercial", "Administrador"],
+    },
+];
+
+export default routes;
