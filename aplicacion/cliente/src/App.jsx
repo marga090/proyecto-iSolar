db175c48a223a4ff31bd65b49dca6e82db873624
Angel <angeldzrz3103@gmail.com>
2025-03-31 10:58:31 +0200

    fondo añadido en la aplicacion y boton de cerrar sesion mejorado

--- aplicacion/cliente/src/App.jsx
+++ aplicacion/cliente/src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import Logo from "./images/logo.png";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthProvider";
 import Inicio from "./pages/Inicio";
@@ -13,23 +13,22 @@ function App() {
 
   return (
     <BrowserRouter>
-      <div className="insene">
-        <div className="logo">
+      <div className="cabecera d-flex">
+        <div className="columna izquierda"></div>
+        <div className="columna logo">
           <img src={Logo} alt="Insene" />
         </div>
-
-        {authData && (
-          <div className="d-flex justify-content-end">
-            <button className="btn btn-danger btn-sm cerrar-sesion" onClick={cerrarSesion}>
+        <div className="columna sesion">
+          {authData && (
+            <button className="btn btn-warning btn-sm cerrar-sesion" onClick={cerrarSesion}>
               Cerrar Sesión
             </button>
-          </div>
-        )}
+          )}
+        </div>
       </div>
 
       <Routes>
         <Route path="/" element={<Inicio />} />
-
         {routes.map(({ path, element, roles }) => (
           <Route key={path} element={<PrivateRoute allowedRoles={roles} />}>
             <Route path={path} element={element} />
