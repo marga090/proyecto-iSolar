c69e6345be98df87929be707449cd07fd8e448ea
Angel <angeldzrz3103@gmail.com>
2025-04-01 15:51:21 +0200

    mejora del formulario de trabajador

--- aplicacion/cliente/src/pages/FormularioTrabajador.jsx
+++ aplicacion/cliente/src/pages/FormularioTrabajador.jsx
@@ -1,140 +1,103 @@
 import '../styles/Formularios.css';
-import { useEffect, useCallback, useMemo } from "react";
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useCallback, useMemo } from "react";
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
 import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Form as BootstrapForm, Button } from 'react-bootstrap';
 import Axios from "../axiosConfig";
+import useDocumentTitle from '../components/Titulo';
+import { erroresSweetAlert2 } from '../utils/erroresSweetAlert2';
+import CamposFormulario from '../components/CamposFormulario';
 
 export default function FormularioTrabajador() {
-    useEffect(() => {
-        document.title = "Formulario de trabajador";
-    }, []);
+    useDocumentTitle("Formulario de Trabajador");
 
-    const redirigir = useNavigate();
+    const navigate = useNavigate();
 
-    const initialValues = { nombre: "", contrasena: "", telefono: "", rol: "", equipo: "", subequipo: "", };
+    const initialValues = useMemo(() => ({
+        nombre: "", contrasena: "", telefono: "", rol: "", equipo: "", subequipo: "",
+    }), []);
 
-    const validationSchema = useMemo(() => Yup.object({
+    const validationSchema = Yup.object({
         nombre: Yup.string().required("Este campo es obligatorio"),
         contrasena: Yup.string().required("Este campo es obligatorio"),
         telefono: Yup.string().matches(/^\d{9}$/, "El teléfono debe tener 9 dígitos").required("Este campo es obligatorio"),
         rol: Yup.string().required("Este campo es obligatorio"),
         equipo: Yup.string().required("Este campo es obligatorio"),
         subequipo: Yup.string().required("Este campo es obligatorio"),
-    }), []);
-
+    });
 
     const onSubmit = useCallback(async (values, { setSubmitting, resetForm }) => {
         try {
             const response = await Axios.post("/registrarTrabajador", values);
+
             Swal.fire({
                 icon: "success",
                 title: `El ID de trabajador de ${response.data.nombreTrabajador} es: ${response.data.idTrabajador}`,
                 text: "Datos registrados correctamente",
                 confirmButtonText: "Vale"
-            }).then(() => redirigir(-1));
-
+            }).then(() => navigate(-1));
             resetForm();
         } catch (error) {
-            if (error.response) {
-                const mensajeError = error.response?.data?.error || "Hubo un problema con la solicitud. Inténtalo de nuevo.";
-                Swal.fire({
-                    icon: "warning",
-                    title: "Error",
-                    text: mensajeError,
-                    confirmButtonText: "Vale"
-                });
-            } else if (error.message.includes("Network Error") || error.message.includes("ERR_CONNECTION_REFUSED")) {
-                Swal.fire({
-                    icon: "question",
-                    title: "Error de Conexión",
-                    text: "Verifica tu conexión a internet e inténtalo de nuevo",
-                    confirmButtonText: "Vale"
-                });
-            } else {
-                Swal.fire({
-                    icon: "error",
-                    title: "Error",
-                    text: "Ocurrió un error inesperado. Inténtalo de nuevo",
-                    confirmButtonText: "Vale"
-                });
-            }
+            erroresSweetAlert2(error);
         } finally {
             setSubmitting(false);
         }
-    }, []);
+    }, [navigate]);
 
     return (
         <Container fluid="md" className="trabajador">
             <h1 className="text-center mb-4">Registrar un Trabajador</h1>
             <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
-                {({ errors, touched, isSubmitting }) => (
+                {({ errors, touched, isSubmitting, isValid }) => (
                     <Form as={BootstrapForm} className="p-4 border rounded shadow-sm bg-light">
                         <Row className="mb-3">
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Nombre completo del trabajador *</BootstrapForm.Label>
-                                    <Field name="nombre" type="text" className={`form-control ${errors.nombre && touched.nombre ? "is-invalid" : ""}`} placeholder="Ej: Carlos Martínez Gómez" />
-                                    <ErrorMessage name="nombre" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Nombre completo del trabajador *" name="nombre" type="text" placeholder="Ej: Carlos Martínez Gómez"
+                                    tooltip="Introduce el nombre y apellidos del trabajador" errors={errors} touched={touched} />
                             </Col>
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Contraseña *</BootstrapForm.Label>
-                                    <Field name="contrasena" type="text" className={`form-control ${errors.contrasena && touched.contrasena ? "is-invalid" : ""}`} placeholder="Ej: 3jjh48721&nsk" />
-                                    <ErrorMessage name="contrasena" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Contraseña para el trabajador *" name="contrasena" type="text"
+                                    tooltip="Introduce la contraseña que tendrá el trabajador" errors={errors} touched={touched} />
                             </Col>
                         </Row>
 
                         <Row className="mb-3">
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Teléfono *</BootstrapForm.Label>
-                                    <Field name="telefono" type="tel" className={`form-control ${errors.telefono && touched.telefono ? "is-invalid" : ""}`} placeholder="Ej: 666555444" />
-                                    <ErrorMessage name="telefono" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Teléfono del trabajador *" name="telefono" type="tel" placeholder="Ej: 600000000"
+                                    tooltip="Introduce el número de teléfono del trabajador (9 dígitos)" errors={errors} touched={touched} />
                             </Col>
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Equipo *</BootstrapForm.Label>
-                                    <Field name="equipo" type="text" className={`form-control ${errors.equipo && touched.equipo ? "is-invalid" : ""}`} placeholder="Ej: Comercial" />
-                                    <ErrorMessage name="equipo" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Equipo del trabajador *" name="equipo" type="text" placeholder="Ej: Equipo 1"
+                                    tooltip="Introduce el equipo al que pertenecerá el trabajador" errors={errors} touched={touched} />
                             </Col>
                         </Row>
 
                         <Row className="mb-3">
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Subequipo *</BootstrapForm.Label>
-                                    <Field name="subequipo" type="text" className={`form-control ${errors.subequipo && touched.subequipo ? "is-invalid" : ""}`} placeholder="Ej: Telemarketing" />
-                                    <ErrorMessage name="subequipo" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Subequipo del trabajador *" name="subequipo" type="text" placeholder="Ej: Subequipo 1"
+                                    tooltip="Introduce el subequipo al que pertenecerá el trabajador" errors={errors} touched={touched} />
                             </Col>
+
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Rol *</BootstrapForm.Label>
-                                    <Field as="select" name="rol" className={`form-control ${errors.rol && touched.rol ? "is-invalid" : ""}`} >
-                                        <option value="">Seleccione un rol</option>
-                                        <option value="Administrador">Administrador</option>
-                                        <option value="Captador">Captador</option>
-                                        <option value="Comercial">Comercial</option>
-                                        <option value="Coordinador">Coordinador</option>
-                                        <option value="Instalador">Instalador</option>
-                                        <option value="Recursos_Humanos">Recursos Humanos</option>
-                                        <option value="Tramitador">Tramitador</option>
-                                    </Field>
-                                    <ErrorMessage name="rol" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Rol *" name="rol" as="select"
+                                    tooltip="Selecciona el rol que tendrá el trabajador" errors={errors} touched={touched} >
+                                    <option value="">Selecciona una opción</option>
+                                    <option value="Administrador">Administrador</option>
+                                    <option value="Captador">Captador</option>
+                                    <option value="Comercial">Comercial</option>
+                                    <option value="Coordinador">Coordinador</option>
+                                    <option value="Instalador">Instalador</option>
+                                    <option value="Recursos_Humanos">Recursos Humanos</option>
+                                    <option value="Tramitador">Tramitador</option>
+                                </CamposFormulario>
                             </Col>
                         </Row>
 
                         <div className="d-flex justify-content-center">
-                            <Button type="submit" className="mt-3" disabled={isSubmitting}>
-                                {isSubmitting ? "Registrando..." : "Registrar Trabajador"}
+                            <Button type="submit" className="mt-3" disabled={isSubmitting || !isValid} aria-label="Registrar trabajador" >
+                                {isSubmitting ? "Enviando..." : "Registrar Trabajador"}
                             </Button>
                         </div>
                     </Form>
