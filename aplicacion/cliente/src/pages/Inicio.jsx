b15d1bb81c92876328b50e4e74776fceed38cb21
Angel <angeldzrz3103@gmail.com>
2025-04-02 10:42:59 +0200

    mejora del formulario de inicio de sesion

--- aplicacion/cliente/src/pages/Inicio.jsx
+++ aplicacion/cliente/src/pages/Inicio.jsx
@@ -1,29 +1,30 @@
 import '../styles/Formularios.css';
-import { useEffect, useMemo, useCallback } from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { useMemo, useCallback } from 'react';
+import { Formik, Form } from 'formik';
 import * as Yup from 'yup';
-import Swal from 'sweetalert2';
 import { useNavigate } from 'react-router-dom';
 import { Container, Row, Col, Form as BootstrapForm, Button } from 'react-bootstrap';
 import { useContext } from 'react';
 import { AuthContext } from "../context/AuthProvider";
 import Axios from "../axiosConfig";
+import useDocumentTitle from '../components/Titulo';
+import { erroresSweetAlert2 } from '../utils/erroresSweetAlert2';
+import CamposFormulario from '../components/CamposFormulario';
 
 export default function Inicio() {
-    const redirigir = useNavigate();
+    useDocumentTitle("Inicio de Sesión");
 
-    const initialValues = { idTrabajador: '', contrasena: '' };
-
-    const validationSchema = useMemo(() => Yup.object({
-        idTrabajador: Yup.number().integer().required("Este campo es obligatorio").min(1, "El ID no es válido"),
-        contrasena: Yup.string().required("Este campo es obligatorio"),
-    }), []);
-
+    const navigate = useNavigate();
     const { iniciarSesion: iniciarSesionContext } = useContext(AuthContext);
 
-    useEffect(() => {
-        document.title = "Inicio de Sesión";
-    }, []);
+    const initialValues = useMemo(() => ({
+        idTrabajador: '', contrasena: ''
+    }), []);
+
+    const validationSchema = Yup.object({
+        idTrabajador: Yup.number().integer().required("Este campo es obligatorio").min(1, "El ID no es válido"),
+        contrasena: Yup.string().required("Este campo es obligatorio"),
+    });
 
     const onSubmit = useCallback(async (values, { setSubmitting, resetForm }) => {
         try {
@@ -35,68 +36,41 @@ export default function Inicio() {
             const tipoTrabajador = response.data.tipoTrabajador;
 
             if (tipoTrabajador === 'Captador') {
-                redirigir('/captadores');
+                navigate('/captadores');
             } else if (tipoTrabajador === 'Comercial') {
-                redirigir('/comerciales');
+                navigate('/comerciales');
             } else if (tipoTrabajador === 'Administrador') {
-                redirigir('/administradores');
+                navigate('/administradores');
+            } else {
+                navigate('/');
             }
         } catch (error) {
-            if (error.response) {
-                const mensajeError = error.response?.data?.error || "Hubo un problema con la solicitud. Inténtalo de nuevo.";
-                Swal.fire({
-                    icon: "warning",
-                    title: "Error",
-                    text: mensajeError,
-                    confirmButtonText: "Vale"
-                });
-            } else if (error.message.includes("Network Error") || error.message.includes("ERR_CONNECTION_REFUSED")) {
-                Swal.fire({
-                    icon: "question",
-                    title: "Error de Conexión",
-                    text: "Verifica tu conexión a internet e inténtalo de nuevo",
-                    confirmButtonText: "Vale"
-                });
-            } else {
-                Swal.fire({
-                    icon: "error",
-                    title: "Error",
-                    text: "Ocurrió un error inesperado. Inténtalo de nuevo",
-                    confirmButtonText: "Vale"
-                });
-            }
+            erroresSweetAlert2(error);
         } finally {
             setSubmitting(false);
         }
-    }, []);
+    }, [navigate]);
 
     return (
         <Container fluid="md" className="inicioSesion">
             <h1 className="text-center mb-4">Inicio de Sesión</h1>
             <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit} >
-                {({ isSubmitting, errors, touched }) => (
+                {({ isSubmitting, errors, touched, isValid }) => (
                     <Form as={BootstrapForm} className="p-4 border rounded shadow-sm bg-light">
-
                         <Row className="mb-3">
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>ID de Trabajador *</BootstrapForm.Label>
-                                    <Field name="idTrabajador" type="number" className={`form-control ${errors.idTrabajador && touched.idTrabajador ? "is-invalid" : ""}`} placeholder="Ej: 4" />
-                                    <ErrorMessage name="idTrabajador" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="ID de Trabajador *" name="idTrabajador" type="number" placeholder="Ej: 5"
+                                    tooltip="Introduce tu código único de trabajador" errors={errors} touched={touched} />
                             </Col>
                             <Col xs={12} md={6}>
-                                <BootstrapForm.Group>
-                                    <BootstrapForm.Label>Contraseña *</BootstrapForm.Label>
-                                    <Field name="contrasena" type="password" className={`form-control ${errors.contrasena && touched.contrasena ? "is-invalid" : ""}`} placeholder="Introduce tu contraseña" />
-                                    <ErrorMessage name="contrasena" component="div" className="text-danger" />
-                                </BootstrapForm.Group>
+                                <CamposFormulario label="Contraseña *" name="contrasena" type="password"
+                                    tooltip="Introduce tu contraseña" errors={errors} touched={touched} />
                             </Col>
                         </Row>
 
                         <div className="d-flex justify-content-center">
-                            <Button type="submit" className="mt-3" disabled={isSubmitting}>
-                                {isSubmitting ? "Cargando..." : "Iniciar Sesión"}
+                            <Button type="submit" className="mt-3" disabled={isSubmitting || !isValid} aria-label="Registrar feedback" >
+                                {isSubmitting ? "Iniciando sesión..." : "Iniciar Sesión"}
                             </Button>
                         </div>
                     </Form>
