49840cc4373891413c658fd795d72618362a2903
Angel <angeldzrz3103@gmail.com>
2025-04-02 13:00:56 +0200

    mejora del panel de captador

--- aplicacion/cliente/src/pages/PanelCaptador.jsx
+++ aplicacion/cliente/src/pages/PanelCaptador.jsx
@@ -1,25 +1,27 @@
 import "../styles/Paneles.css";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
 import { Container, Row, Col, Button } from "react-bootstrap";
+import useDocumentTitle from '../components/Titulo';
 
 export default function PanelCaptador() {
-    useEffect(() => {
-        document.title = "Panel de Captador";
-    }, []);
+    useDocumentTitle("Panel de Captador");
+
+    const BotonesNavegacion = () => (
+        <Row className="g-3 justify-content-center">
+            <Col xs={12} sm={6} md={3} lg={3} className="d-flex justify-content-center">
+                <Button as={Link} to="/captadores/contacto" variant="primary" className="custom-button" aria-label="Registrar un nuevo contacto" > Registrar Contacto </Button>
+            </Col>
+            <Col xs={12} sm={6} md={3} lg={3} className="d-flex justify-content-center">
+                <Button as={Link} to="/captadores/visita" variant="primary" className="custom-button" aria-label="Registrar una nueva visita" > Registrar Visita </Button>
+            </Col>
+        </Row>
+    );
 
     return (
         <Container fluid="md" className="captador">
             <h1 className="text-center mb-4">Panel de Captadores</h1>
 
-            <Row className="g-3 justify-content-center">
-                <Col xs={12} sm={6} md={3} lg={3} className="d-flex justify-content-center">
-                    <Button as={Link} to="/captadores/contacto" variant="primary" className="custom-button" aria-label="Crear un nuevo contacto" > Crear Contacto </Button>
-                </Col>
-                <Col xs={12} sm={6} md={3} lg={3} className="d-flex justify-content-center">
-                    <Button as={Link} to="/captadores/visita" variant="primary" className="custom-button" aria-label="Registrar una nueva visita" > Registrar Visita </Button>
-                </Col>
-            </Row>
+            <BotonesNavegacion />
         </Container>
     );
 }
