152bcbfcac6640aa240968102f4407f2d36ef316
Angel <angeldzrz3103@gmail.com>
2025-04-25 12:35:34 +0200

    Dockerización de la aplicación

--- aplicacion/servidor/config/dbConfig.js
+++ aplicacion/servidor/config/dbConfig.js
@@ -8,6 +8,7 @@ const db = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
+    charset: 'utf8mb4',
     connectionLimit: 10,
 }).promise();
 
